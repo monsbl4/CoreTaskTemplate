b881204c6118609fa115992f76045933c5690c80
Denisov Sergey <sedenisov@mail.ru>
2021-07-20 21:25:58 +0300

    first

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,17 +1,17 @@
 package jm.task.core.jdbc;
-
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-
 public class Main {
+
     private static final UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
         userService.createUsersTable();
-        userService.saveUser("Serg","Denisov", (byte) 32);
-        userService.saveUser("Oleg","Ivanov", (byte) 34);
-        userService.saveUser("Alex","Petrov",(byte) 29);
-        userService.saveUser("Olga","Sidorova", (byte) 24);
+        userService.saveUser("Serg", "Denisov", (byte) 32);
+        userService.saveUser("Oleg", "Ivanov", (byte) 34);
+        userService.saveUser("Alex", "Petrov", (byte) 29);
+        userService.saveUser("Olga", "Sidorova", (byte) 24);
         userService.getAllUsers();
         userService.cleanUsersTable();
         userService.dropUsersTable();
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,13 +1,15 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDao userDao = new UserDaoJDBCImpl();
+    //  UserDao userDao = new UserDaoJDBCImpl();
+    UserDao userDao = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
         userDao.createUsersTable();
@@ -19,7 +21,7 @@ public class UserServiceImpl implements UserService {
 
     public void saveUser(String name, String lastName, byte age) {
         userDao.saveUser(name, lastName, age);
-        System.out.println("User с именем - "+ name + " добавлен в базу данных");
+        System.out.println("User с именем - " + name + " добавлен в базу данных");
     }
 
     public void removeUserById(long id) {
